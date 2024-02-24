c384fc97e50d5e7108e696be5037453cfc49c385
Rafał Romanowicz <rrrafalon@gmail.com>
2024-02-24 19:00:24 +0100

    Task2: Scene, TurnBar
    Replaced 'Game' scene with 'GameCombat' scene which serves purpose of testing combat

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -9,15 +9,16 @@
 		[SerializeField] private string name;
 		[SerializeField] private int value;
 		//Please see: ItemUsage.cs (Command Pattern)
-		[SerializeField] private ItemUsage[] onUse;
+		[SerializeField] private ItemUsage[] onUse = System.Array.Empty<ItemUsage>();
 
 		public string Name => name;
 		public int Value => value;
 
-		public Item(string name, int value)
+		public Item(string name, int value, ItemUsage[] onUse = null)
 		{
 			this.name = name;
 			this.value = value;
+			this.onUse = onUse;
 		}
 
 		public void Use(InventoryController targetInventory)
