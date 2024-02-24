17b9361ae78965cc856692fdd944b40aacc1bab2
Rafał Romanowicz <rrrafalon@gmail.com>
2024-02-24 18:50:11 +0100

    Task 1: Optimization, cleanup, layer mask
    Optimized:
    * usage of Camera.main by camera reference bound through inspector
    * usage of Text Mesh Pro UI label by reference bound through inspector
    * .text should be SetText, less unnecessary allocation
    * Item spawning mechanism in Update(), can be dispatched to separate Coroutine which is not called every frame
    * Removed usage of FindObjectOfType -> it's slow
    * Money label updating mechanism refactored into event based, there is no need to do that each frame!
    
    Commentary proposition for usage of async/await approach
    
    Moved const layer mask to inspector
    Cleaned up inspector properties a bit

--- Assets/Scripts/Items/ItemsManager.cs
+++ Assets/Scripts/Items/ItemsManager.cs
@@ -1,57 +1,103 @@
 ﻿namespace AFSInterview.Items
 {
+	using System.Collections;
 	using TMPro;
 	using UnityEngine;
 
 	public class ItemsManager : MonoBehaviour
 	{
+		[Header("References")]
+		[SerializeField] private TMP_Text moneyUILabel = null;
 		[SerializeField] private InventoryController inventoryController;
-		[SerializeField] private int itemSellMaxValue;
 		[SerializeField] private Transform itemSpawnParent;
 		[SerializeField] private GameObject itemPrefab;
 		[SerializeField] private BoxCollider itemSpawnArea;
+		[SerializeField] private Camera viewCamera;
+
+		[Header("Settings")]
+		[SerializeField] private LayerMask pickupMask;
+		[SerializeField] private int itemSellMaxValue;
 		[SerializeField] private float itemSpawnInterval;
 
-		private float nextItemSpawnTime;
-		
+		//In case of Async/Await approach
+		//private CancellationTokenSource cancellationToken = new();
+
+		private void Start()
+		{
+			inventoryController.onMoneyChange += OnMoneyChange;
+			OnMoneyChange();
+
+			//moved this code from Update(), it's unnecessary to be updated each frame
+			StartCoroutine(SpawnItemRoutine());
+		}
+
+		private void OnDestroy()
+		{
+			//In case of Async/Await approach
+			//cancellationToken.Cancel();
+		}
+
 		private void Update()
 		{
-			if (Time.time >= nextItemSpawnTime)
-				SpawnNewItem();
-			
-			if (Input.GetMouseButtonDown(0))
+			if(Input.GetMouseButtonDown(0))
 				TryPickUpItem();
-			
-			if (Input.GetKeyDown(KeyCode.Space))
-				inventoryController.SellAllItemsUpToValue(itemSellMaxValue);
 
-			FindObjectOfType<TextMeshProUGUI>().text = "Money: " + inventoryController.Money;
+			if(Input.GetKeyDown(KeyCode.Space))
+				inventoryController.SellAllItemsUpToValue(itemSellMaxValue);
 		}
+
+		private void OnMoneyChange()
+		{
+			//it would be nice to do nullcheck
+			//or
+			//use some kind of [Required] attribute to ensure that inspector warns developer of not bound reference
+			//	I assume that developer (me) does the reference binding in the inspector right after compilation in Unity
+			moneyUILabel.SetText($"Money: {inventoryController.Money}");
+		}
+
+		private IEnumerator SpawnItemRoutine()
+		{
+			var waiter = new WaitForSeconds(itemSpawnInterval);
+			while(true)
+			{
+				SpawnNewItem();
+				yield return waiter;
+			}
+		}
+		/*	//Async/Await approach
+		private async void SpawnItemTask()
+		{
+			while(!cancellationToken.IsCancellationRequested)
+			{
+				SpawnNewItem();
+				await Task.Delay(Mathf.FloorToInt(itemSpawnInterval * 1000));
+			}
+		}
+		*/
 
 		private void SpawnNewItem()
 		{
-			nextItemSpawnTime = Time.time + itemSpawnInterval;
-			
 			var spawnAreaBounds = itemSpawnArea.bounds;
-			var position = new Vector3(
+			Vector3 position = new(
 				Random.Range(spawnAreaBounds.min.x, spawnAreaBounds.max.x),
 				0f,
 				Random.Range(spawnAreaBounds.min.z, spawnAreaBounds.max.z)
 			);
-			
+
 			Instantiate(itemPrefab, position, Quaternion.identity, itemSpawnParent);
 		}
 
 		private void TryPickUpItem()
 		{
 			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			var layerMask = LayerMask.GetMask("Item");
-			if (!Physics.Raycast(ray, out var hit, 100f, layerMask) || !hit.collider.TryGetComponent<IItemHolder>(out var itemHolder))
+			if (!Physics.Raycast(ray, out var hit, 100f, pickupMask)
+			|| !hit.collider.TryGetComponent<IItemHolder>(out var itemHolder)
+			)
 				return;
 			
 			var item = itemHolder.GetItem(true);
-            inventoryController.AddItem(item);
-            Debug.Log("Picked up " + item.Name + " with value of " + item.Value + " and now have " + inventoryController.ItemsCount + " items");
+			inventoryController.AddItem(item);
+			Debug.Log($"Picked up {item.Name} with value of {item.Value} and now have {inventoryController.ItemsCount} items");
 		}
 	}
 }
